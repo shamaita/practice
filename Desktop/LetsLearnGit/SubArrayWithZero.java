86b58d786e685b0f0ff3a35d11d8779ae867b8bb
Shamaita Pal <palshamaita1998@gmail.com>
2021-09-21 23:43:53 +0530

    initial commit

---
+++ Desktop/LetsLearnGit/SubArrayWithZero.java
@@ -0,0 +1,10 @@
+package bruteforce;
+
+public class SubArrayWithZero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Desktop/LetsLearnGit/Sum.java
@@ -0,0 +1,13 @@
+package bruteforce;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int a=4;
+ int b=7;
+ int c=a+b;
+ System.out.println(c);
+	}
+
+}
